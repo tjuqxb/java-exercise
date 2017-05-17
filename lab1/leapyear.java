ddf15b830b3d98e0eeab1758846a69f22a1f7985
qxb <382935477@qq.com>
2017-05-17 16:12:04 +0800

    complete leapyear.java

---
+++ lab1/leapyear.java
@@ -0,0 +1,9 @@
+class LeapYear{
+	public static void main(String args[]){
+		int year = 2000;
+		if (year%400 == 0 || (year%4 == 0 && year%100 != 0)){
+			System.out.println (year + "is a leap year.");
+		}
+	
+	}
+}
